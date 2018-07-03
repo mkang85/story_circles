8945e08a47467b8b836bcd4a4334d3b78e86264b
Michael Kang <michaelkang85@gmail.com>
2018-07-03 08:41:00 -0400

    created gem files, basic layout

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    #enable :sessions
+  end
+
+  get '/' do
+    erb :'/layout'
+  end
+end
