9c5fce7a90b404f88eaf07f4a04b661e676de3de
Michael Kang <michaelkang85@gmail.com>
2018-07-06 21:50:04 -0400

    added slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :circles
   has_secure_password
+
+  def slug
+  username.downcase.split(" ").join("-")
+end
+
+def self.find_by_slug(slug)
+  User.all.find{|user| user.slug == slug}
+end
+
 end
